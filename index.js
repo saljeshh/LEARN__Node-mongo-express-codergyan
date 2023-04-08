b67ea9ce79897309b6a0267774bbc96a603e4ff6
saljeshh <saljesh2020@gmail.com>
2023-04-08 17:05:20 +0545

    finished chapter 1, npm , pacakge json

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ console.log(sub);
 
 */
 
+/*
 // using ecma script module like react
 import { sum, diff } from "./lib.js";
 
@@ -20,3 +21,30 @@ const diffs = diff(2, 34);
 console.log(sums);
 
 console.log(diffs);
+*/
+
+// using fs module
+const fs = require("fs");
+
+// this is synchronous so it mustnt be used in server as server isnt blocked
+// const txt = fs.readFileSync("demo.txt", "utf-8");
+// console.log(txt);
+
+// this is asynchronous , so response is in callback
+
+// testing performace
+const t1 = performance.now();
+console.log(5 + 7);
+
+/*
+    when we pass only one argument in callback it means its error
+    as first is (err, data) is format
+*/
+
+fs.readFile("demo.txt", "utf-8", (err, text) => {
+  console.log(text);
+});
+const t2 = performance.now();
+console.log(9 + 7);
+
+console.log(t2 - t1);
